7709cf9419b21497f8a8be10638ea3b51af80758
MLB L'original <111900469+Mlbking50@users.noreply.github.com>
2024-12-24 17:23:23 +0100

    create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+document.getElementById('androidb').addEventListener("click", function() {
+  window.location.href ='android.html';
+});
+
+document.getElementById('iosb').addEventListener("click", function() {
+  window.location.href = 'iOS.html';
+});
+
+document.getElementById('windowsb').addEventListener("click", function() {
+  window.location.href = 'Windows.html';
+});
+
+document.getElementById('telegramb').addEventListener("click", function() {
+  window.location.href = 'https://www.t.me/mlb_065';
+});
+
+document.getElementById('ppssppb').addEventListener("click", function() {
+  window.location.href = 'ppsspp.html';
+});
+
+document.getElementById('fb').addEventListener("click", function(){
+  const fb = 'https://www.facebook.com/mlb.loriginal';
+  
+  window.location.href = fb;
+});
+
+document.getElementById('gv').addEventListener("click", function(event){
+  const gv = 'https://download1322.mediafire.com/u9a62wzui4agnPvQZTAm6Tch4kudnE3KFwvN-XA5Z7fPG_9IFPtw395HfzABFvW701Rr4sQCCZMs5upuOcedyLVnfzqwtDkZGqFkHn1vJHM8jY2qE4mk6T3lOYeiF0YzcPzWw4zeuk7yzpcU-9fjqJWaiMxYjfZcCFF0VSzKS57WUA/y79bc74rbqfy64x/GV-WoC780h.xapk';
+  window.location.href = gv;
+});
+
+document.getElementById('cyb').addEventListener("click", function(event){
+  const cyb = 'magnet:?xt=urn:btih:A81F44DB938E364D1655F72816BF6404E4DE16EA&dn=Cyberpunk+2077%3A+Ultimate+Edition+%28v2.2+%2B+All+DLCs+%2B+Dev+Tools+%2B+Bonus+Content+%2B+MULTi19%29+%28From+61+GB%29+%5BDODI+Repack%5D&tr=udp%3A%2F%2F9.rarbg.to%3A2870%2Fannounce&tr=udp%3A%2F%2Feddie4.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Fthetracker.org%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.wasabii.com.tw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.cypherpunks.ru%3A6969%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce';
+  
+  window.location.href = cyb;
+});
+
+document.getElementById('mlbb').addEventListener("click", function(event){
+  const mlbb = 'https://filecr.com/file-download/?id=020811070000#';
+  
+  window.location.href = mlbb;
+});
