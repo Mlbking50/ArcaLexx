c0f52b6623feac2a273fb24a218b19017030ffb7
MLB L'original <111900469+Mlbking50@users.noreply.github.com>
2024-12-24 17:23:55 +0100

    create android.js

---
+++ android.js
@@ -0,0 +1,23 @@
+document.getElementById('shadowslayerb').addEventListener("click", function(event) {
+  const link1 = 'https://filecr.com/file-download/?id=020811070000#';
+  alert('Download Shadow Slayer');
+  window.location.href = link1;
+});
+
+document.getElementById('mlbb-b').addEventListener("click", function(event) {
+  const link2 = 'https://filecr.com/file-download/?id=020811070000#';
+  alert('Download Mobile legends: bang bang');
+  window.location.href = link2;
+});
+
+document.getElementById('gangster-vegasb').addEventListener("click", function (event){
+  const link3 = 'https://download1322.mediafire.com/u9a62wzui4agnPvQZTAm6Tch4kudnE3KFwvN-XA5Z7fPG_9IFPtw395HfzABFvW701Rr4sQCCZMs5upuOcedyLVnfzqwtDkZGqFkHn1vJHM8jY2qE4mk6T3lOYeiF0YzcPzWw4zeuk7yzpcU-9fjqJWaiMxYjfZcCFF0VSzKS57WUA/y79bc74rbqfy64x/GV-WoC780h.xapk';
+  alert('Download Gangster vegas');
+  window.location.href = link3;
+});
+
+document.getElementById('standoff2b').addEventListener("click", function(event) {
+  const link4 = 'https://filecr.com/file-download/?id=020811070000#';
+ alert('Download Stand Off 2'); 
+  window.location.href = link4;
+});
